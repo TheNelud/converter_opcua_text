7d5e449ebec968fcdba5b98a05137a1461d542ae
TheNelud <riwok@bk.ru>
2022-08-15 14:39:11 +0300

    доработал тэг жизни сервера

--- converter/server.py
+++ converter/server.py
@@ -2,6 +2,7 @@ import time
 
 from . import logInfo, parser
 from opcua import Server, ua
+import threading
 
 
 class Server_OPCUA_txt:
@@ -10,38 +11,42 @@ class Server_OPCUA_txt:
         self.config = parser.get_config()
         self.flag = 0
 
+        # Конфигурация сервера
         self.server = Server()
         self.server.set_endpoint(self.config['UA_HOST'])
         self.server.set_server_name(self.config["UA_SERVER_NAME"])
         self.idx = self.server.register_namespace(self.config["UA_ROOT_NAMESPACE"])
         self.myobj = self.server.nodes.objects.add_folder(self.idx, "DATA")
 
-    def life_server_tag(self, tag):
-        try:
-            if self.flag != 1:
-                self.flag = 1
-                tag.set_value(True)
-            else:
-                self.flag = 0
-                tag.set_value(False)
-        except Exception:
-            self.logging.warning("Сервер мертв, пошел на перезапуск")
-            self.restart()
+        # Пульсирующий тэг(SERVER LIFE)
+        self.tagLifeServer = self.myobj.add_variable(self.idx, 'Life Server', False, ua.VariantType.Boolean)
+
+    def life_server_tag(self):
+        while True:
+            time.sleep(5)
+            try:
+                if self.flag != 1:
+                    self.flag = 1
+                    self.tagLifeServer.set_value(True)
+                else:
+                    self.flag = 0
+                    self.tagLifeServer.set_value(False)
+            except Exception:
+                self.logging.warning("Сервер мертв, пошел на перезапуск")
+                self.restart()
 
     def restart(self):
         self.logging.warning('Остановка сервера')
         self.server.stop()
+        time.sleep(10)
         self.logging.warning('Запуск сервера')
         self.start()
 
     def start(self):
 
-        self.tagLifeServer = self.myobj.add_variable(self.idx,
-                                                     'Life Server',
-                                                     False,
-                                                     ua.VariantType.Boolean)
-        self.server.start()
-        while True:
-
-            self.life_server_tag(self.tagLifeServer)
-            time.sleep(5)
+        try:
+            self.server.start()
+            self.life_server_tag()
+        except KeyboardInterrupt:
+            self.logging.warning('Закрытие программы')
+        self.server.stop()
